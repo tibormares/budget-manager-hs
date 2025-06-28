f31ea0bd456eb346fb2796ecb88a67d4bb940011
Tibor Mares <tibor.mares0@gmail.com>
2025-06-28 11:47:42 +0200

    Added function: counting total from user's input.

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
 package budget;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        double total = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            list.add(line);
+        }
+
+        for (String s : list) {
+            Pattern pattern = Pattern.compile("([\\w']*\\s+)+([$])(\\d+[.]\\d{2})");
+            Matcher matcher = pattern.matcher(s);
+            if (matcher.find()) {
+                System.out.println(matcher.group(0));
+                total += Double.parseDouble(matcher.group(3));
+            }
+        }
+
+        System.out.printf("\nTotal: $%.2f", total);
     }
 }
